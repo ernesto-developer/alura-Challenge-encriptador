a4adb7053d0c7d586b3f8780c889bba56975596d
ERE <ernesto_711@live.com.mx>
2022-01-21 00:04:46 -0600

    cambios

---
+++ app.js
@@ -0,0 +1,13 @@
+var area = document.querySelector(".textoDentroDeCaja");
+var boton = document.querySelector(".btnEncriptar");
+
+function imprimir(){
+
+    console.log("ola");
+
+}
+
+
+boton.onclik =  imprimir;
+
+
